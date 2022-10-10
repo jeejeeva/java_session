ccf9e8999ed6dc686d53e2c0d4a9ee0861301c21
jeeva <jeejeeva>
2022-10-10 15:19:04 +0530

    Calculate program

---
+++ Employee.java
@@ -0,0 +1,18 @@
+class Employee
+{
+    static String address = "7, neru street, chennai";
+    static int companyOpenTi = 10;
+    String empName;
+    int empID;
+
+public static void main(String[] args)
+{
+    Employee emp1 = new Employee();
+    emp1.empName = "venkiy";
+    emp1.empID = 7826;
+    System.out.println(emp1.empName);
+    Employee emp2 = new Employee();
+    
+}
+
+}
